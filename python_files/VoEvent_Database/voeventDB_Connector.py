75fb0b205d8314876d73b798e2d2e5b1e8c91b8c
Landon Patmore <landon.patmore@gmail.com>
2016-11-03 03:35:46 -0400

    restructured

---
+++ python_files/VoEvent_Database/voeventDB_Connector.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import voeventdb.remote as vr
+import voeventdb.remote.apiv1 as apiv1
+from voeventdb.remote.apiv1 import FilterKeys, OrderValues
+from voeventdb.remote.helpers import Synopsis
+from datetime import datetime
+import pytz
+from mysql_connector import insert_transient
+
+"""
+@author: Landon Patmore
+"""
+def send_data():
+    print("Sending Data...")
+    my_filters = {
+        FilterKeys.role:'observation',
+        FilterKeys.stream:'voevent.4pisky.org/GAIA',
+        FilterKeys.authored_since:datetime(2016,10,31,tzinfo=pytz.UTC)
+    }
+
+    swift_bat_grb_list = apiv1.list_ivorn(
+        filters=my_filters,
+        order=OrderValues.author_datetime_desc,
+        n_max=1
+    )
+    swift_bat_grb_ivorn = swift_bat_grb_list[0]
+    swift_bat_grb_ivorn
+
+    # Retrieve a 'synopsis' (nested dictionary) for the VOEvent packet,
+    grb_nested_dict = apiv1.packet_synopsis(swift_bat_grb_ivorn)
+    # And convert it to a user-friendly class-object:
+    grb_synopsis = Synopsis(grb_nested_dict)
+
+    sky_event=grb_synopsis.sky_events[0]
+
+    insert_transient(grb_synopsis.ivorn, grb_synopsis.author_ivorn, sky_event.position.ra.deg, sky_event.position.dec.deg, grb_synopsis.author_datetime, grb_synopsis.received, grb_synopsis.role)
+    print("Done sending data.")
+
+    raw_xml = apiv1.packet_xml(swift_bat_grb_ivorn)
+    print(raw_xml[:72])
+    print(' ... ')
+    with open('swift_voevent11.xml','w') as f:
+        f.write(raw_xml)
+
+send_data()
---
+++ transientGO/app/src/main/java/com/csc380team10/transientgo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.csc380team10.transientgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
